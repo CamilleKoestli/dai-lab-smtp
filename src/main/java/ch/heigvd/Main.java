337df9f519ae830dcac4ae96cb959440b29205cf
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-12-04 17:20:37 +0100

    update main according to other changes

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -4,6 +4,7 @@ import ch.heigvd.smtp.*;
 import ch.heigvd.fileio.MessageManager;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,37 +15,53 @@ public class Main {
             // Création d'un groupe d'e-mails avec MessageManager
             MessageManager messageManager = new MessageManager();
             List<EmailGroup> emailGroups = messageManager.getGroupMails("src/main/java/ch/heigvd/config/emails.utf8");
+            /*for(EmailGroup x : emailGroups){
+                System.out.println("Sender : " + x.getSender());
+                for(String y : x.getReceivers()){
+                    System.out.println("Receivers :");
+                    System.out.println(y);
+                }
+            }*/
 
-            // Sélectionner le premier groupe
+/*            // Sélectionner le premier groupe
             EmailGroup emailGroup = emailGroups.get(0);
-            System.out.println("sender: " + emailGroup.getSender() + ", receivers: " + emailGroup.getReceivers());  //TODO
+            System.out.println("sender: " + emailGroup.getSender() + ", receivers: " + emailGroup.getReceivers());  //TODO*/
 
             // Création d'un message avec MessageManager
             List<Message> messages = messageManager.readContentFromFile("src/main/java/ch/heigvd/config/messages.utf8");
             Collections.shuffle(messages);
             // Select the first (random) message
-            Message selectedMessage = messages.get(0);      //TODO change
+            //Message selectedMessage = messages.get(0);      //TODO change
+            int mIdx = 0;
+            List<Email> emails = new ArrayList<>();
 
-            for(Message m : messages){
+            for(EmailGroup eg : emailGroups){
+                emails.add(new Email(eg, messages.get(mIdx)));
+                mIdx = ++mIdx % messages.size();
+            }
+            /*for(Message m : messages){
                 System.out.println("subject: "
                         + m.getSubject() + ", body: " + m.getBody());   //TODO
-            }
-
+            }*/
+/*
             Email email = new Email(emailGroup, selectedMessage);
             System.out.println("sender: " + email.getSender() + ", receivers: " + email.getReceivers() + ", subject: "
-                    + email.getSubject() + ", body: " + email.getBody());   //TODO
+                    + email.getSubject() + ", body: " + email.getBody());   //TODO*/
 
             // Création d'un client SMTP avec le serveur et le port appropriés
             SMTPClient smtpClient = new SMTPClient("127.0.0.1", 1025);
 
-            smtpClient.connect();
             // Envoi du mail à chaque destinataire
-            for (String receiver : emailGroup.getReceivers()) {
-                System.out.println("Dans boucle for...");
-                smtpClient.sendMail(emailGroup.getSender(), emailGroup.getReceivers(), email.getSubject(), email.getBody());
-                System.out.println("sender: " + email.getSender() + ", receivers: " + email.getReceivers() + ", subject: "
-                        + email.getSubject() + ", body: " + email.getBody());
-                smtpClient.receiveResponse();
+            for(Email e : emails) {
+                System.out.println("---------------------- MAIL ---------------------------");
+                /*for (String receiver : e.getReceivers()) {
+                    System.out.println("Dans boucle for...");
+                    smtpClient.sendMail(e.getSender(), e.getReceivers(), e.getSubject(), e.getBody());
+                    System.out.println("sender: " + e.getSender() + ", receivers: " + e.getReceivers() + ", subject: "
+                            + e.getSubject() + ", body: " + e.getBody());
+                    smtpClient.receiveResponse();
+                }*/
+                smtpClient.sendMail(e.getSender(), e.getReceivers(), e.getSubject(), e.getBody());
             }
             smtpClient.disconnect();
 
