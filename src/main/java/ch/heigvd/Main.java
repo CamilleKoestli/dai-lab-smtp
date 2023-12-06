833c95527043ae4206574e977e599926ae88d40c
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-12-06 22:32:22 +0100

    change main according to last project modifications

--- src/main/java/ch/heigvd/Main.java
+++ src/main/java/ch/heigvd/Main.java
@@ -1,42 +1,25 @@
 package ch.heigvd;
 
+import ch.heigvd.config.Configuration;
 import ch.heigvd.smtp.*;
-import ch.heigvd.fileio.MessageManager;
+import ch.heigvd.config.ConfigManager;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            // Création d'un groupe d'e-mails avec MessageManager
-            MessageManager messageManager = new MessageManager();
-            List<EmailGroup> emailGroups = messageManager.getGroupMailsFromJsonFile("src/main/java/ch/heigvd/config/emails.json");
 
-            // Création d'un message avec MessageManager
-            List<Message> messages = messageManager.readMessagesFromJsonFile("src/main/java/ch/heigvd/config/messages.json");
-            Collections.shuffle(messages);
+            ConfigManager configManager = new ConfigManager(Configuration.NB_GROUP, Configuration.MESSAGES_FILE, Configuration.VICTIMS_FILE);
 
-            int mIdx = 0;
-            List<Email> emails = new ArrayList<>();
+            SMTPClient smtpClient = new SMTPClient(Configuration.HOST_ADDRESS, Configuration.PORT_NB);
 
-            for(EmailGroup eg : emailGroups){
-                emails.add(new Email(eg, messages.get(mIdx)));
-                mIdx = ++mIdx % messages.size();
+            for(int i = 0; i < configManager.nbGroups; ++i){
+                smtpClient.sendMail(new Email(configManager.emailGroups.get(i), configManager.messages.get(i)));
             }
-
-            // Création d'un client SMTP avec le serveur et le port appropriés
-            SMTPClient smtpClient = new SMTPClient("127.0.0.1", 1025);
-
-            // Envoi du mail à chaque destinataire
-            for(Email e : emails) {
-                System.out.println("---------------------- MAIL ---------------------------");
-                smtpClient.sendMail(e.getSender(), e.getReceivers(), e.getSubject(), e.getBody());
-            }
-            smtpClient.disconnect();
 
         } catch (IOException e) {
             e.printStackTrace();
