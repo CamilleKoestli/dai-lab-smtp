253cf5c22c43ac4d565296074a096e683c2ecced
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-12-07 01:05:07 +0100

    add email pattern regex for email address validation

--- src/main/java/ch/heigvd/config/Configuration.java
+++ src/main/java/ch/heigvd/config/Configuration.java
@@ -6,10 +6,8 @@ public class Configuration {
     public final static String HOST_ADDRESS = "127.0.0.1";
     public final static String VICTIMS_FILE = "src/main/java/ch/heigvd/config/victims.json";
     public final static String MESSAGES_FILE = "src/main/java/ch/heigvd/config/messages.json";
-
     public static final int MIN_GROUP_SIZE = 2;
     public static final int MAX_GROUP_SIZE = 5;
+    public static final String EMAIL_PATTERN = "^([A-Za-z0-9.]+)@([A-Za-z0-9]+)\\.([A-Za-z0-9]+)$";
 
-    //TODO get nb_group from terminal
-    public static final int NB_GROUP = 3;
 }
